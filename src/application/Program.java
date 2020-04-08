7a26862be09126bd3f819f69d97fc7e73649ae77
Junior Selister <junior.selister@gmail.com>
2020-04-08 13:00:35 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,7 +16,7 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		List<ChessPiece> captured = new ArrayList<ChessPiece>();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while (true) {
 			try {
